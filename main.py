a36915c288c096fe0a50c7aeb86c870858abbc8d
link-fgfgui <fgfgui_link@outlook.com>
2025-04-26 19:21:20 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+import pythonmonkey as pm
+from pprint import pp
+js_code = open('Avatar.js', 'r', encoding='utf-8').read()
+pm.eval(js_code)
+avatar_raw = list(pm.globalThis._avatar)
+texts = json.loads(open("m20250221hy2bnoalts-zh-cn.json",
+                   'r', encoding='utf-8').read())
+avatar = {str(int(i["_id"])): i["Name"] for i in avatar_raw}
+avatar.update(json.loads(
+    open("avatar_add.json", "r", encoding='utf-8').read()))
+# pp(avatar)
+# quit()
+# print(avatar)
+# print(len(avatar))
+conversationIdsPairTemp = {}
+for key in texts:
+    key_slice = key.split('_')
+    conversationId = key_slice[2]
+    avatarId = key_slice[3]
+
+    if conversationId == "play":
+        print(
+            f"开拓者 与 {avatar[avatarId]} |\n\t{avatar[avatarId]}: {texts[key]}")
+    else:
+        if conversationId in conversationIdsPairTemp.keys():
+            print(
+                f"{avatar[avatarId]} 与 {avatar[conversationIdsPairTemp[conversationId]["id"]]} |")
+            print(
+                f"\t{avatar[conversationIdsPairTemp[conversationId]["id"]]}: {conversationIdsPairTemp[conversationId]["text"]}")
+            print(f"\t{avatar[avatarId]}: {texts[key]}")
+        else:
+            conversationIdsPairTemp[conversationId] = {
+                "text": texts[key], "id": avatarId}
---
+++ run.md
@@ -0,0 +1,5 @@
+```sh
+pip install pythonmonkey
+wget https://homdgcat.wiki/data/CH/Avatar.js
+python main.py
+```
